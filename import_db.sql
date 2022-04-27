a19c8b0f51ca6bfaa5ca1c81416495f057aad079
Kevin <NateraKevin1@gmail.com>
2022-04-27 16:58:01 -0400

    added insert statements to seed database

--- import_db.sql
+++ import_db.sql
@@ -2,7 +2,7 @@
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
-    lname TEXT,
+    lname TEXT NOT NULL,
 );
 
 CREATE TABLE questions (
@@ -13,3 +13,22 @@ CREATE TABLE questions (
 
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+
+INSERT INTO 
+    users(fname,lname)
+VALUES
+    ('Kevin','Natera'),
+    ('Some','Person'),
+    ('Anon','Guy'),
+    ('Random','Human');
+
+INSERT INTO
+    questions(title,body,user_id)
+VALUES
+    ('Sort Question', 'Why is Bubble Sort trash?', (SELECT id FROM users WHERE fname = 'Kevin')),
+    ('Random Question 342', 'weiurwhrewhoew', (SELECT id FROM users WHERE fname = 'Some')),
+    ('Random Question 983765', 'sjfbdskfnwnlfe', (SELECT id FROM users WHERE fname = 'Anon')),
+    ('Random Question 098765', 'djfbckjlnfew', (SELECT id FROM users WHERE fname = 'Random')),
+    ('Random Question 0987654', 'dslkvmfwperkwoifwe?', (SELECT id FROM users WHERE fname = 'Kevin')),
+    ('Random Question 98765', 'weoifhoejfoiwnfiownl', (SELECT id FROM users WHERE fname = 'Anon'));
